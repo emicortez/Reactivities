d4b5084a2333db44d012dd674fb951dd4f064ad3
Emiliano Cortez <emiliano.cortez@dgenix.com>
2020-05-23 12:25:52 -0300

    Added user activity entity

--- 3.Application/Activities/Create.cs
+++ 3.Application/Activities/Create.cs
@@ -2,9 +2,11 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -40,16 +42,18 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccesor _userAccesor;
+
+            public Handler(DataContext context, IUserAccesor userAccesor)
             {
                 _context = context;
+                _userAccesor = userAccesor;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = new Activity
                 {
-
                     Id = new Guid(),
                     Title = request.Title,
                     Description = request.Description,
@@ -60,9 +64,22 @@ namespace Application.Activities
                 };
 
                 _context.Activities.Add(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccesor.GetCurrentUsername());
+
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserActivities.Add(attendee);
+
                 var success = await _context.SaveChangesAsync() > 0;
 
-                if(success) return Unit.Value;
+                if (success) return Unit.Value;
 
                 throw new Exception("Problem saving changes");
             }
---
+++ 3.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using Application.Activities;
+using AutoMapper;
+using Domain;
+
+namespace Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Activity, ActivityDto>();
+            CreateMap<UserActivity, AttendeeDto>()
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName));
+        }
+    }
+}
